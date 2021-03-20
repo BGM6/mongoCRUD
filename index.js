40164ab76213bb3df993c4b2ba9524d96969501d
bguilas <bryanjguilas@gmail.com>
2021-03-19 22:43:15 -0700

    added edit and delete

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Post = require('./models/post');
+const methodOverride = require('method-override');
 const path = require('path');
 const PORT = 5000;
-const app = express();
-
 //mongo connection
 mongoose.connect('mongodb://localhost:27017/blogDb', {
 		useNewUrlParser: true,
@@ -18,10 +17,12 @@ db.on('error', console.error.bind(console, 'CONNECTION OPEN'));
 db.once('open', () => {
 		console.log('DATABASE CONNECTED')
 })
+const app = express();
 
 //Middleware
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(methodOverride('_method'));
 
 //ejs
 app.set('view engine', 'ejs');
@@ -51,6 +52,31 @@ app.get('/posts/:id', async (req, res) => {
 		const post = await Post.findById(id);
 		res.render('posts/show', { post })
 })
+
+//Edit
+app.get('/posts/:id/edit', async (req, res) => {
+		const { id } = req.params;
+		const post = await Post.findById(id);
+		res.render('posts/edit', { post })
+})
+
+app.put('/posts/:id', async (req, res) => {
+		const { id } = req.params;
+		const {title, text} = req.body;
+		await Post.findByIdAndUpdate(id, {title, text}, {
+				runValidators: true,
+				new: true
+		})
+		res.redirect('/posts')
+})
+
+//Delete
+app.delete('/posts/:id', async (req, res) => {
+		const {id} = req.params;
+		await Post.findByIdAndDelete(id);
+		res.redirect('/posts')
+})
+
 
 app.listen(PORT, () => {
 		console.log(`Server listening on port ${PORT}`);
